661e577cc2051cd23a70a61e97b893415a25ff8f
AnnaGorina <49533436+AnnaGorina@users.noreply.github.com>
2019-10-11 21:13:18 +0300

    Update Company.cpp

--- Lab1/Company.cpp
+++ Lab1/Company.cpp
@@ -1,27 +1,21 @@
 #include "company.h"
 #include "person.h"
 
-Company::Company(std::string NameOfCompany)
-{
+Company::Company(std::string NameOfCompany) {
     this->NameOfCompany = NameOfCompany;
 }
 
-const std::string &Company::get_company() const
-{
+std::string& Company::get_company() {
     return NameOfCompany;
 }
 
-Person& Company::GetNameOfPerson(unsigned i) const
-{
+Person& Company::GetNameOfPerson(uint16_t i) const {
     return (*PersonOfCompany[i]);
 }
 
-const Person* Company::GetPerson(const std::string &Surname, const std::string &Name, const std::string &Patronymic) const
-{
-    for (uint16_t i = 0; i < PersonOfCompany.size(); i++)
-    {
-        if (PersonOfCompany[i]->get_name_person() == Name && PersonOfCompany[i]->get_surname_person() == Surname && PersonOfCompany[i]->get_patronymic_person() == Patronymic)
-        {
+Person* Company::GetPerson(const std::string &Surname, const std::string &Name, const std::string &Patronymic) const {
+    for (uint16_t i = 0; i < PersonOfCompany.size(); i++) {
+        if (PersonOfCompany[i]->get_name_person() == Name && PersonOfCompany[i]->get_surname_person() == Surname && PersonOfCompany[i]->get_patronymic_person() == Patronymic) {
             return PersonOfCompany[i];
         }
     }
@@ -29,12 +23,9 @@ const Person* Company::GetPerson(const std::string &Surname, const std::string &
     return nullptr;
 }
 
-bool Company::CheckPerson(const std::string &Surname, const std::string &Name, const std::string &Patronymic)
-{
-    for (uint16_t i = 0; i < PersonOfCompany.size(); i++)
-    {
-        if (PersonOfCompany[i]->get_name_person() == Name && PersonOfCompany[i]->get_surname_person() == Surname && PersonOfCompany[i]->get_patronymic_person() == Patronymic)
-        {
+bool Company::CheckPerson(const std::string &Surname, const std::string &Name, const std::string &Patronymic) {
+    for (uint16_t i = 0; i < PersonOfCompany.size(); i++) {
+        if (PersonOfCompany[i]->get_name_person() == Name && PersonOfCompany[i]->get_surname_person() == Surname && PersonOfCompany[i]->get_patronymic_person() == Patronymic) {
             return true;
         }
     }
@@ -42,63 +33,54 @@ bool Company::CheckPerson(const std::string &Surname, const std::string &Name, c
     return false;
 }
 
-unsigned Company::GetCountPerson()
-{
+uint16_t Company::GetCountPerson() {
     return PersonOfCompany.size();
 }
 
-bool Company::JoinAPerson(Person& person)
-{
-    if(person.GetCountCompany() != 5)
-    {
-       for (uint16_t i = 0; i < person.GetCountCompany(); i++)
-       {
-           if (person.GetNameOfCompany(i) == NameOfCompany)
-           {
-               return false;
-           }
-       }
+bool Company::JoinAPerson(Person* person) {
+    if(person->GetCountCompany() <= 5) {
+        for(uint16_t i = 0; i < this->GetCountPerson(); i++) {
+            if(PersonOfCompany[i] == person) {
+                return false;
+            }
+        }
 
-        person.setCompany(NameOfCompany);
-        PersonOfCompany.push_back(&person);
-
+        PersonOfCompany.push_back(person);
+        person->JoinACompany(this);
         return true;
     }
+
+    return false;
 }
 
-bool Company::DismissPerson(Person &person)
-{
-    std::vector <Person*>::iterator it = PersonOfCompany.begin();
+bool Company::DismissPerson(Person* person) {
+    std::vector<Person*>::iterator it = PersonOfCompany.begin();
 
-    while (true)
-    {
-        if (*it == &person)
-        {
+    for(uint16_t i = 0; i < PersonOfCompany.size(); i++) {
+        if(*it == person) {
             PersonOfCompany.erase(it);
-            person.LeaveCompany(this->get_company());
+            person->LeaveCompany(this);
+            //PersonOfCompany.erase(std::remove(PersonOfCompany.begin(), PersonOfCompany.end(), &person), PersonOfCompany.end());
             return true;
         }
 
-        if (it == PersonOfCompany.end())
-        {
-            return false;
-        }
-
-        ++it;
+        it++;
     }
+
+    return false;
 }
 
 Company::~Company() = default;
 
-std::ostream &operator<<(std::ostream &stream, Company &company)
-{
+std::ostream &operator<<(std::ostream &stream, Company &company) {
     stream << "******************************************************" << std::endl;
     stream << "Компания: " << company.get_company() << std::endl;
     stream << "ФИО работников: " << std::endl;
-    for (uint16_t i = 0; i < company.GetCountPerson(); i++)
-    {
+
+    for (uint16_t i = 0; i < company.GetCountPerson(); i++) {
         stream << company.GetNameOfPerson(i).get_surname_person() << " " << company.GetNameOfPerson(i).get_name_person() << " " << company.GetNameOfPerson(i).get_patronymic_person() << std::endl;
     }
+
     stream << "******************************************************" << std::endl;
 
     return stream;
