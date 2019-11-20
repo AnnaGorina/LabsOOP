237545b41f44343c0ed13ea0f84ea81a34bd26b5
AnnaGorina <49533436+AnnaGorina@users.noreply.github.com>
2019-11-20 20:32:32 +0300

    Create Main.java

---
+++ Lab2/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Person FirstPerson = new Person("Иванов", "Иван", "Иванович");
+
+	    Company Microsoft = new Company("Microsoft", "Петров", "Петр", "Петрович");
+    }
+}
