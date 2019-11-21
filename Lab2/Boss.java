70f69a66ae52e75235f687b13a138b48e2f50548
AnnaGorina <49533436+AnnaGorina@users.noreply.github.com>
2019-11-21 15:22:21 +0300

    Update Boss.java

--- Lab2/Boss.java
+++ Lab2/Boss.java
@@ -5,40 +5,30 @@ import java.util.ArrayList;
 public class Boss extends Person {
 
     private Company company;
-    private ArrayList<Employee> PersonOfCompany = new ArrayList<Employee>();
 
-    public Boss (String Surname, String Name, String Patronymic) {
+    public Boss (String Surname, String Name, String Patronymic, Company company) {
         super(Surname, Name, Patronymic);
-
+        this.company = company;
     }
 
-//    public int GetCountPerson (Company company) {
-//        return company.GetCountPerson();
-//    }
 
     public boolean JoinAPerson (Employee employee) {
 
         if (employee.GetCountCompany() < 5) {
-            for (int i = 0; i < company.GetCountPerson(); i++) {
-                if (PersonOfCompany.get(i) == employee) {
-                    return false;
-                }
+            if (company.isPerson(employee)) {
+                return false;
             }
 
-            PersonOfCompany.add(employee);
+            company.addEmployee(employee);
             employee.JoinACompany(this);
-            company.JoinAPerson(employee);
             return true;
         }
 
         return false;
     }
 
-    public boolean DismissPerson (Employee employee) {
-        for (int i = 0; i < PersonOfCompany.size(); i++) {
-            PersonOfCompany.remove(i);
-        }
-
-        return false;
+    public void DismissPerson (Employee employee) {
+        company.removeEmployee(employee);
     }
 }
+
